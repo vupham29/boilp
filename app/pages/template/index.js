31d3e60e8a0f25d649197bc13959368d256d2572
vupham <vuquangpham2909@gmail.com>
2023-03-05 21:52:36 +0700

    fix(app): generate the right template

--- app/pages/template/index.js
+++ app/pages/template/index.js
@@ -1,7 +1,33 @@
-import Base from "@/pages/template/base";
+import Page from '@/classes/Page';
+import Aside from "@/components/Aside";
 
-export default class extends Base{
+export default class extends Page{
     constructor(){
-        super();
+        super({
+            element: '.lesson',
+        });
+    }
+
+    create(){
+        super.create();
+
+        this.id = this.element?.getAttribute('data-lesson');
+
+        // not lessons page
+        if(!this.id) return;
+
+        // create instance
+        const instanceName = this.id;
+
+        import(`./${instanceName}.js`)
+            .then((instance) => {
+                console.log(instance);
+                this.lesson = new instance.default({
+                    el: this.element,
+                    canvas: this.element.querySelector('#canvas')
+                });
+            });
+
+        this.asideElement = new Aside();
     }
 }
