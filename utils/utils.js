e272177e4850e0177603a1ac4461ebd261403327
vuquangpham <vuquangpham2909@gmail.com>
2023-08-07 11:15:25 +0700

    chore: refactor

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const {VIEW_ENGINE} = require('./configs');
 
 /**
  * Check folder exist
@@ -59,9 +60,74 @@ const stringToSlug = (string) => {
         .toLowerCase();
 };
 
+
+/**
+ * Create pages prototype
+ * */
+const extensionEngine = '.' + VIEW_ENGINE;
+const createPagesPrototype = (prototypes = []) => prototypes.map(prototype => {
+    const {pages} = prototype;
+
+    const appPath = path.join('app', 'pages', prototype.base);
+    const viewEnginePath = path.join('views', prototype.base);
+
+    pages.forEach((page, i) => {
+        if(!page.id){
+            page.id = stringToSlug(page.title);
+        }
+        const templateName = `template`;
+
+        // template doesn't exist => simply return, not create the prototype for it
+        if(!isPathExistSync(appPath, templateName, '.js')){
+            // create directory first
+            createDirectory(appPath);
+
+            // template path
+            const templatePath = path.join(appPath, '..', 'template');
+
+            cloneFile({
+                source: path.join(templatePath, 'index.js'),
+                destination: path.join(appPath, 'index' + '.js')
+            });
+
+            cloneFile({
+                source: path.join(templatePath, 'template.js'),
+                destination: path.join(appPath, templateName + '.js')
+            });
+        }
+
+        // create directory
+        createDirectory(path.join(appPath, page.id));
+
+        // clone the template for the new-id
+        cloneFile({
+            source: path.join(appPath, templateName + '.js'),
+            destination: path.join(appPath, page.id, 'index' + '.js')
+        }); // app file
+
+        // view engine template
+        if(!isPathExistSync(viewEnginePath, templateName, extensionEngine)){
+            // create directory first
+            createDirectory(viewEnginePath);
+
+            // template path
+            const templatePath = path.join(viewEnginePath, '..', 'template');
+
+            cloneFile({
+                source: path.join(templatePath, `template${extensionEngine}`),
+                destination: path.join(viewEnginePath, templateName + extensionEngine)
+            });
+        }
+
+        cloneFile({
+            source: path.join(viewEnginePath, templateName + extensionEngine),
+            destination: path.join(viewEnginePath, page.id + extensionEngine)
+        }); // engine file
+    });
+
+    return prototype;
+});
+
 module.exports = {
-    isPathExistSync,
-    cloneFile,
-    createDirectory,
-    stringToSlug
+    createPagesPrototype
 };
